b3c6dff0dc84a408e03e65597d78453a5d6e9262
Efrem Ropelato <efrem.ropelato@gmail.com>
2024-11-03 09:30:09 +0100

    fix runner & switch to vitejs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
+import './index.css'
 import App from './Dashboard';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
